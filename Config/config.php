a65258388354031356d16ba9338a8c81575cdbaf
MauricioVz <mvelasquez58@misena.edu.co>
2015-04-07 18:46:05 -0500

    File Config Modified
    Se ha caMBIADO EL archivo config q habia sido modificado con
    anterioridad para ser subido al hosting a modo de pruba, a partir de
    este punto entra en la fase final el proyecto.
    Falta: La gestion de los ingredientes, Las recetas y sus clasificaciones
    Tambien falta un menu dinamico que sera hecho despues de la fecha de
    entrega del proyecto con un motivo personal dfe aprendizaje

--- Config/config.php
+++ Config/config.php
@@ -2,8 +2,8 @@
 
 	define("DRIVER", "mysql");
 	define("PORT", "3306");
-	define("SERVER", "mysql.nixiweb.com");
-	define("DBNAME", "u857820495_recet");
-	define("USER", "u857820495_root");
+	define("SERVER", "localhost");
+	define("DBNAME", "bd_recetas");
+	define("USER", "root");
 	define("PASSWORD", "elcarro");
  ?>
